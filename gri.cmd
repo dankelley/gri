3ffd56d55d8be7bb63b0de2a07ff084f899ff336
dankelley <dankelley>
2000-05-22 13:40:56 +0000

    Use zlib to permit reading compressed data files.

--- gri.cmd
+++ gri.cmd
@@ -1,4 +1,4 @@
-#         gri - scientific graphic program (version 2.5.2)
+#         gri - scientific graphic program (version 2.5.3)
 #              GPL Copyright 1991-2000 Dan E. Kelley.
 #
 # NOTE: The linkages to `extern "C"' routines makes use a list of C 
