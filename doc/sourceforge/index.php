e8d24b0632756f29fabb3b98b8ad0639c2d4d937
dankelley <dankelley>
2001-03-24 15:40:53 +0000

    Trying different location of material, to be cleaner. Add connection to 2.6.0 pre-releases.

--- doc/sourceforge/index.php
+++ doc/sourceforge/index.php
@@ -1,86 +1,106 @@
       <!-- -*- html -*- -->
 
 <!-- colors (copied from SF colors):
-  dark, under headings headings:	#737b9c
-  light, table background		#EEEEF8
+  dark, under headings headings:        #737b9c
+  light, table background               #EEEEF8
 -->
 
 <html>
   <head>
     <title>[Gri] Gri Site</title>
-    <meta name="Copyright" content="2000 Dan Kelley, Dalhousie University, Halifax, Nova Scotia, Canada">
+    <meta name="Copyright" content="2001 Dan Kelley, Dalhousie University, Halifax, Nova Scotia, Canada">
     <meta name="Author" content="Dan Kelley, Dan.Kelley@Dal.Ca">
     <meta name="keywords" content="Gri, graphics, Scientific Computing">
     <LINK rel="stylesheet" href="./gri.css" type="text/css">
   </head>
   
   <body bgcolor=#FFFFFF topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" marginheight="0" marginwidth="0">
+
     <TABLE width="100%" border=0 cellspacing=0 cellpadding=0 bgcolor="#737b9c">
       <TR>
         <td align="left" width="50%">
-	  <SPAN class=maintitlebar><A class="maintitlebar" href="./"><B>Home</B></A></span>
-	</td>
-	<td align="right" width="50%"><SPAN class=maintitlebar><IMG src="./images/blank.gif" height="1" width="145" border=0 alt=""></span></td>
-      </TR>
-    </TABLE>
+          <SPAN class=maintitlebar><A class="maintitlebar" href="http://sourceforge.net/"><IMG SRC="./sflogo.png" NAME="Graphic3" ALIGN=MIDDLE BORDER=0></A></span>
+        </td>
+        <td align="right" width="50%"><SPAN class=maintitlebar><IMG src="./images/blank.gif" height="1" width="145" border=0 alt=""></span></td>
+      </tr>
+    </table>
 
     <table bgcolor="#EEEEF8" width="100%" border=0 cellspacing=0 cellpadding=0>
-        <TR bgcolor="#EEEEF8">
-	<td align="center">
-	  <font size="+4">Gri: a language for scientific illustration</font>
-	</td>
+      <TR bgcolor="#EEEEF8">
+        <td align="center">
+          <font size="+4">Gri: a language for scientific illustration</font>
+        </td>
       </tr>
     </table>
     
     <table width="100%" cellspacing=0 cellpadding="2" border=0>
       <tr>
-	<td valign="top" width="165" bgcolor="#EEEEF8">  <!-- light color -->
-
-	  <!-- Menus -->
-	  <table width="100%" cellspacing=0 border=0 cellpadding=2 bgcolor="#EEEEF8">
-	    <tr>
-	      <TD align="center" bgcolor="#737b9c">
-		<IMG src="./images/blank.gif" height="1" width="1" border=0 alt=""><BR>
-		<span class="titlebar">
-<font color="#ffffff">Documentation</font></span>
-	      </td> 
-	    </tr>
-	    <tr>
-	      <td align="right" class="menus">
-		<A class="menus" HREF="./gridoc/html/index.html">HTML manual (vsn 2.6.0)</A><br>
-		<A class="menus" HREF="./gri.pdf">PDF manual (vsn 2.6.0)</A><br>
-		<A class="menus" HREF="./gridoc/html/FAQ.html">FAQ</A><br>
-		<A class="menus" HREF="./gri-cookbook/index.html">Cookbook of Examples</A><br>
+        <td valign="top" width="165" bgcolor="#EEEEF8">  <!-- light color -->
+          <table width="100%" cellspacing=0 border=0 cellpadding=2 bgcolor="#EEEEF8">
+            <tr>
+              <TD align="center" bgcolor="#737b9c">
+                <IMG src="./images/blank.gif" height="1" width="1" border=0 alt=""><BR>
+                <span class="titlebar">
+                <font color="#ffffff">Documentation</font></span>
+              </td> 
+            </tr>
+            <tr>
+              <td align="right" class="menus">
+                <A class="menus" HREF="./gridoc/html/index.html">HTML manual (vsn 2.6.0)</A><br>
+                <A class="menus" HREF="./gri.pdf">PDF manual (vsn 2.6.0)</A><br>
+                <A class="menus" HREF="./gridoc/html/FAQ.html">FAQ</A><br>
+                <A class="menus" HREF="./gri-cookbook/index.html">Cookbook of Examples</A><br>
                 <A  class="menus" HREF="http://www2.linuxjournal.com/lj-issues/issue75/3743.html">Article in Linux Journal</A><br>
-	      </td>
-	    </tr>
-	    <tr>
-	      <TD align="center" bgcolor="#737b9c">
-		<IMG src="./images/blank.gif" height="1" width="1" border=0 alt=""><BR>
-		<span class="maintitlebar"><font color="#ffffff">Download (Unstable)</font></span>
-	      </td> </tr>
-	    <tr><td class="menus" align="right">
-		<b>2000-Jun-21</b><br><A  class="menus" HREF="http://download.sourceforge.net/gri/gri-2.5.5.tgz">gri-2.5.5.tgz</A><br>
-		<A  class="menus" HREF="http://download.sourceforge.net/gri/gri-2.5.5-1.i386.rpm">gri-2.5.5-1.i386.rpm</A><br>
-	      </td></tr>
-	    
-	    <tr>
-	      <TD align="center" bgcolor="#737b9c">
-	        <IMG src="./images/blank.gif" height="1" width="1" border=0 alt=""><BR>
-		<span class="maintitlebar"><font color="#ffffff">Download (Stable)</font></span>
-		</td> </tr>
-		<tr><td class="menus" align="right">
-		<b>2001-???-??</b><br><font color="red">gri-2.6.0 coming soon</font><br>
-		<b>2000-May-22</b><br><A  class="menus" HREF="http://download.sourceforge.net/gri/gri-2.4.4.tgz">gri-2.4.4.tgz</A><br>
-		<A  class="menus" HREF="http://download.sourceforge.net/gri/gri-2.4.4-1.i386.rpm">gri-2.4.4-1.i386.rpm</A><br>
-		<A  class="menus" HREF="http://www.debian.org/Packages/stable/math/gri.html">gri-2.4.2-1.deb</A><br>
-		</td></tr>
-	  </table>
-	</TD>
-	<!-- Main Window -->
-	<TD VALIGN="top" ALIGN="left" >
-	  <table align="center" width="95%"><tr><td>
-	  
+              </td>
+            </tr>
+            
+	    <!-- DOWNLOAD STABLE -->
+            <tr>
+              <TD align="center" bgcolor="#737b9c">
+                <IMG src="./images/blank.gif" height="1" width="1" border=0 alt=""><BR>
+                <span class="maintitlebar"><font color="#ffffff">Download stable release</font></span>
+                </td> </tr>
+                <tr><td class="menus" align="right">
+                <b>2001-???-??</b><br><font color="red">gri-2.6.0 coming soon</font><br>
+                <b>2000-May-22</b><br><A  class="menus" HREF="http://download.sourceforge.net/gri/gri-2.4.4.tgz">gri-2.4.4.tgz</A><br>
+                <A  class="menus" HREF="http://download.sourceforge.net/gri/gri-2.4.4-1.i386.rpm">gri-2.4.4-1.i386.rpm</A><br>
+                <A  class="menus" HREF="http://www.debian.org/Packages/stable/math/gri.html">gri-2.4.2-1.deb</A><br>
+              </td>
+            </tr>
+
+	    <!-- DOWNLOAD UNSTABLE -->
+            <tr> 
+              <TD align="center" bgcolor="#737b9c">
+                <IMG src="./images/blank.gif" height="1" width="1" border=0 alt=""><BR>
+                <span class="maintitlebar"><font color="#ffffff">Download unstable release</font></span>
+              </td>
+            </tr>
+            <tr>
+              <td class="menus" align="right">
+                <b>2000-Jun-21</b><br><A  class="menus" HREF="http://download.sourceforge.net/gri/gri-2.5.5.tgz">gri-2.5.5.tgz</A><br>
+                <A  class="menus" HREF="http://download.sourceforge.net/gri/gri-2.5.5-1.i386.rpm">gri-2.5.5-1.i386.rpm</A><br>
+              </td>
+            </tr>
+            
+	    <!-- DOWNLOAD DEVELOPMENT -->
+            <tr> 
+              <TD align="center" bgcolor="#737b9c">
+                <IMG src="./images/blank.gif" height="1" width="1" border=0 alt=""><BR>
+                <span class="maintitlebar"><font color="#ffffff">Download prerelease</font></span>
+              </td>
+            </tr>
+            <tr>
+              <td class="menus" align="right">
+                <A class="menus" HREF="http://sourceforge.net/project/showfiles.php?group_id=5511&release_id=28434">2.6.0 pre-releases</a><br>
+              </td>
+            </tr>
+          </table>
+      </td>
+
+      <!-- Main Window -->
+      <TD VALIGN="top" ALIGN="left" >
+      <table align="center" width="95%"><tr><td>
+          
 Gri is an open-source language for scientific graphics programming.
 It is command-driven, as opposed to point/click.  Some users consider
 Gri similar to LaTeX, since both provide extensive power as a reward
@@ -105,25 +125,26 @@ symbols are available in a TeX-like syntax.
 <p>Folks who write 1000-line Gri scripts usually start with something
 as simple as the following.
 <pre>
+<font color="#82140F">
    # Draw a linegraph
    open file.dat
    read columns x y
    draw curve
+</font>
 </pre>
 
-<p>
-
-<hr noshade="noshade">
-    <center>
-    <small>
-      Copyright &copy; 2001 by Dan Kelley<br>
-      This material may be distributed only subject to the terms and conditions set forth in the GNU Publication License
-      </small>
-      </center>
-	<!-- close the main window -->      
-      </TD>
-      </tr>
-</table>
+        <hr noshade="noshade">
+        <center>
+          <small>
+            Copyright &copy; 2001 by Dan Kelley<br>
+            This material may be distributed only subject to the terms and conditions 
+            set forth in the GNU Publication License
+          </small>
+        </center>
+        <!-- close the main window -->      
+            </TD>
+          </tr>
+        </table>
 </td>
 
 <!-- right menus -->
@@ -133,95 +154,89 @@ as simple as the following.
   <table width="100%" border="0" cellspacing="0" cellpadding="0">
     <tr  bgcolor="#737b9c"> 
       <td align="center"> 
-	<span class="maintitlebar"><font color="#ffffff">Status</font></span>
+        <span class="maintitlebar"><font color="#ffffff">Status</font></span>
       </td>
     </tr>
     <tr> 
       <td  class="menubox">
-	<font size="-1">
-	Debugging vsn <font color="red">2.6.0</font><br>
+        <font size="-1">
+        Debugging vsn <font color="red">2.6.0</font><br>
         for release in March or April 2001.
-	</font>
+        </font>
       </td>
     </tr>
   </table>
-  <table width="100%" border="0" cellspacing="0" cellpadding="0">
-      <TR>
-        <TD align="center" bgcolor="#737b9c">
-	  <span class="maintitlebar"><font color="#ffffff">Developer's Corner</font></span>
-        </TD>
-      </TR>
-      <TR>
-      <td class="menus" align="left">
-        <A class="menus" HREF="http://www.sourceforge.net/project/?group_id=5511">SourceForge Gri page</A><br>
-	<A class="menus" HREF="http://www.sourceforge.net/cvs/?group_id=5511">Download CVS source</a><br>
-	<A class="menus" HREF="http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/gri/?cvsroot=gri">Browse CVS history</a><br>
-	<A class="menus" HREF="http://sourceforge.net/pm/task.php?group_project_id=8706&group_id=5511&func=browse">To-do list</A><br>
-	<A class="menus" HREF="http://www.sourceforge.net/patch/?group_id=5511">Patch manager</A><br>
-      </TD>
-    </TR>
-    <TR>
-      <TD align="center" bgcolor="#737b9c">
-        <IMG src="./images/blank.gif" height="1" width="1" border=0 alt=""><BR>
-     </TR>
-  </TABLE>
 
-  <!-- News -->
-  <table width="100%" border="0" cellspacing="0" cellpadding="0">
-    <tr  bgcolor="#737b9c"> 
-      <td align="center"> 
-	<span class="maintitlebar"><font color="#ffffff">News</font></span>
-      </td>
-    </tr>
-    <tr> 
-      <td  class="menus" align="left">
-	<a class="menus" href="http://www.sourceforge.net/news/?group_id=5511">News Forum</a></b>
-      </td>
-    </tr>
-  </table>
+   <!-- News -->
+   <table width="100%" border="0" cellspacing="0" cellpadding="0">
+     <tr  bgcolor="#737b9c"> 
+       <td align="center"> 
+	 <span class="maintitlebar"><font color="#ffffff">News</font></span>
+       </td>
+     </tr>
+     <tr> 
+       <td  class="menus" align="left">
+	 <a class="menus" href="http://www.sourceforge.net/news/?group_id=5511">News Forum</a></b>
+       </td>
+     </tr>
+   </table>
 
   <!-- Contacts -->
   <table width="100%" border="0" cellspacing="0" cellpadding="0">
     <tr bgcolor="#737b9c"> 
       <td align="center"> 
-	    <span class="maintitlebar"><font color="#ffffff">Contacts</font></span>
+            <span class="maintitlebar"><font color="#ffffff">Contacts</font></span>
       </td>
     </tr>
-	    <tr><td class="menus" align="left">
-		<A class="menus" HREF="http://www.sourceforge.net/forum/?group_id=5511">Discussion Forums</A><br>
-		<A class="menus" HREF="http://sourceforge.net/tracker/?group_id=5511">Tracker</A><br>
-		<A class="menus" HREF="http://sourceforge.net/tracker/?atid=105511&group_id=5511&func=browse">Bug Reporter</A><br>
-		<A class="menus" HREF="http://www.sourceforge.net/survey/?group_id=5511">Public Surveys</A><br>
-                <A class="menus" HREF="http://www.sourceforge.net/mail/?group_id=5511">Mailing Lists</A><br> 
-		<A class="menus" HREF="mailto:dankelley@users.sourceforge.net">Email the author</A><br>
-	      </td></tr>
-
-  </table>
-
-  <!-- Hosted by -->
-
-  <table width="100%" border="0" cellspacing="0" cellpadding="0">
-    <tr bgcolor="#737b9c"> 
-      <td align="center"> 
-	    <span class="maintitlebar"><font color="#ffffff">Hosted by</font></span>
-      </td></tr>
-      <tr>
-      <td align="center"   class="menus"> 
-	<A class="menus" HREF="http://sourceforge.net/"><IMG SRC="./sflogo.png" NAME="Graphic3" ALIGN=MIDDLE BORDER=0><br>
+    <tr>
+      <td class="menus" align="left">
+	<A class="menus" HREF="http://www.sourceforge.net/forum/?group_id=5511">Discussion Forums</A><br>
+	<A class="menus" HREF="http://sourceforge.net/tracker/?group_id=5511">Bug/feature tracker</A><br>
+	<A class="menus" HREF="http://sourceforge.net/tracker/?atid=105511&group_id=5511&func=browse">Bug Reporter</A><br>
+	<A class="menus" HREF="http://www.sourceforge.net/survey/?group_id=5511">Public Surveys</A><br>
+	<A class="menus" HREF="http://www.sourceforge.net/mail/?group_id=5511">Mailing Lists</A><br> 
+	<A class="menus" HREF="mailto:dankelley@users.sourceforge.net">Email the author</A><br>
       </td>
     </tr>
   </table>
-  
       
-    </td>
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <TR>
+      <TD align="center" bgcolor="#737b9c">
+        <span class="maintitlebar"><font color="#ffffff">Developer's Corner</font></span>
+      </TD>
+    </TR>
+    <TR>
+      <td class="menus" align="left">
+        <A class="menus" HREF="http://www.sourceforge.net/project/?group_id=5511">SourceForge Gri page</A><br>
+        <A class="menus" HREF="http://www.sourceforge.net/cvs/?group_id=5511">Get CVS source</a><br>
+        <A class="menus" HREF="http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/gri/?cvsroot=gri">View CVS history</a><br>
+        <A class="menus" HREF="http://www.sourceforge.net/patch/?group_id=5511">Patch manager</A><br>
+        <A class="menus" HREF="http://sourceforge.net/pm/task.php?group_project_id=8706&group_id=5511&func=browse">To-do list</A><br>
+      </TD>
+    </TR>
+
+
+  </table>
+
+
+
+
+  </td>
   </tr>
-      </table>
-    <TABLE width="100%" border=0 cellspacing=0 cellpadding=0 bgcolor="737b9c">
-      <TR>
-	<td  align="center"><SPAN class=bottombar>Comments to <A class=bottombar
-	      href="mailto:dankelley@users.sourceforge.net">webmaster</A></span></td>
-	<td align="right" width="200"><SPAN class=bottombar><IMG src="blank.gif" height="1" width="145" border=0 alt=""></span></td>
-      </TR>
-    </TABLE>
-  </BODY>
+  </table>
+
+    <table width="100%" border=0 cellspacing=0 cellpadding=0 bgcolor="737b9c">
+      <tr>
+        <td  align="center"><SPAN class=bottombar><font color="#fffff">Comments to </font> <A class=bottombar
+              href="mailto:dankelley@users.sourceforge.net">webmaster</A></span></td>
+        <td align="right" width="200"><SPAN class=bottombar><IMG src="blank.gif" height="1" width="145" border=0 alt=""></span></td>
+      </tr>
+
+
+
+
+
+    </table>
+  </body>
 </html>
