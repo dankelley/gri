8c306beadcf0b087cb361352e1379b50ed900bcf
psg <psg>
2001-05-11 19:17:03 +0000

    Update Debian pointers

--- doc/sourceforge/index.php
+++ doc/sourceforge/index.php
@@ -73,12 +73,15 @@
                 <tr>
                   <td class="menus" align="right">
 		    <b>General tarball</b><br>
-		    <A class="menus" HREF="http://ftp1.sourceforge.net/gri/gri-2.6.0.tgz">gri-2.6.0.tgz</a><br>
+		    <A class="menus" HREF="http://ftp1.sourceforge.net/gri/gri-2.6.1.tgz">gri-2.6.1.tgz</a><br>
 		    <b>Linux Pre-packaged</b><br>
 		    <A class="menus" HREF="http://ftp1.sourceforge.net/gri/gri-2.6.0-1.src.rpm">gri-2.6.0-1.src.rpm</a><br>
 		    <A class="menus" HREF="http://ftp1.sourceforge.net/gri/gri-2.6.0-1.i386.rpm">gri-2.6.0-1.i386.rpm</a><br>
-		    <A class="menus" HREF="http://ftp1.sourceforge.net/gri/gri_2.6.0-0potato1_i386.deb">gri_2.6.0-0potato1_i386.deb</a><br>
-                    <A  class="menus" HREF="http://packages.debian.org/unstable/science/gri.html">gri-2.6.0 for Debian woody</A><br>
+		    <A class="menus" HREF="http://ftp1.sourceforge.net/gri/gri_2.6.1-0potato1_i386.deb">gri_2.6.1-0potato1_i386.deb</a><br>
+		    <A class="menus" HREF="http://ftp1.sourceforge.net/gri/gri_2.6.1-0woody1_i386.deb">gri_2.6.1-0woody1_i386.deb</a><br>
+                    <A class="menus" HREF="http://packages.debian.org/unstable/science/gri.html">gri-2.6.1 at Debian</A><br>
+                    <A class="menus" HREF="http://packages.debian.org/unstable/doc/gri-html-doc.html">HTML manual at Debian</A><br>
+                    <A class="menus" HREF="http://packages.debian.org/unstable/doc/gri-ps-doc.html">PS manual at Debian</A><br>
 		    <b>Solaris Pre-packaged</b><br>
 		    <A class="menus" HREF="http://ftp1.sourceforge.net/gri/gri-2.6.0-SunOS5.tar.gz">gri-2.6.0-SunOS6.tar.gz</a><br>
 
