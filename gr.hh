53a5dcf0ff2d180e10bc6b589d172d683294d721
dankelley <dankelley>
2001-05-24 16:29:53 +0000

    Thanks to Clyde Clements for pointing this out.

--- gr.hh
+++ gr.hh
@@ -340,7 +340,7 @@ double lapse_rate(double S, double t, double p);
 #endif
 
 // Time type (time_t, int, or long) varies per machine (ug).
-#if defined(VMS) || defined(MSDOS) || defined(IS_DEC_ALPHA)
+#if defined(VMS) || defined(MSDOS) || defined(IS_DEC_ALPHA) || defined(AIX)
 #define SECOND_TYPE time_t
 #else
 #if defined(__DECCXX)
