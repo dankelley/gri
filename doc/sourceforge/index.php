a79b9fa746c5c1075fff1c9960d83aa73197e4dd
dankelley <dankelley>
2001-06-22 18:13:17 +0000

    *** empty log message ***

--- doc/sourceforge/index.php
+++ doc/sourceforge/index.php
@@ -74,10 +74,10 @@
                   <td class="menus" align="right">
 		    <A class="menus" HREF="http://gri.sourceforge.net/gridoc/html/Version_2_6.html#Version2.6">Recent changes</a><br>
 		    <b>General tarball</b><br>
-		    <A class="menus" HREF="http://ftp1.sourceforge.net/gri/gri-2.6.2.tgz">gri-2.6.2.tgz</a><br>
+		    <A class="menus" HREF="http://ftp1.sourceforge.net/gri/gri-2.6.3.tgz">gri-2.6.3.tgz</a><br>
 		    <b>Linux Pre-packaged</b><br>
-		    <A class="menus" HREF="http://ftp1.sourceforge.net/gri/gri-2.6.2-1.src.rpm">gri-2.6.2-1.src.rpm</a><br>
-		    <A class="menus" HREF="http://ftp1.sourceforge.net/gri/gri-2.6.2-1.i386.rpm">gri-2.6.2-1.i386.rpm</a><br>
+		    <A class="menus" HREF="http://ftp1.sourceforge.net/gri/gri-2.6.3-1.src.rpm">gri-2.6.3-1.src.rpm</a><br>
+		    <A class="menus" HREF="http://ftp1.sourceforge.net/gri/gri-2.6.3-1.i386.rpm">gri-2.6.3-1.i386.rpm</a><br>
 		    <A class="menus" HREF="http://ftp1.sourceforge.net/gri/gri_2.6.2-0potato1_i386.deb">gri_2.6.2-0potato1_i386.deb</a><br>
 		    <A class="menus" HREF="http://ftp1.sourceforge.net/gri/gri_2.6.2-1_i386.deb">gri_2.6.2-1_i386.deb</a><br>
                     <A class="menus" HREF="http://packages.debian.org/unstable/science/gri.html">gri-2.6.2 at Debian</A><br>
@@ -188,6 +188,7 @@ as simple as the following.
     <tr> 
       <td  class="menubox">
         <font size="-1">
+        Vsn 2.6.3: 2001-jun-22</br>
         Vsn 2.6.2: 2001-may-20</br>
         Vsn 2.6.1: 2001-may-11</br>
         </font>
