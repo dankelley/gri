2d2fb4d23629ef55204383355d496fb219c0add3
dankelley <dankelley>
2001-08-17 15:08:30 +0000

    *** empty log message ***

---
+++ popt/config.h
@@ -0,0 +1,35 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define if you have the ANSI C header files.  */
+/* #undef STDC_HEADERS */
+
+/* Define to the name of the distribution.  */
+#define PACKAGE "popt"
+
+/* Define to the version of the distribution.  */
+#define VERSION "1.2"
+
+/* Define to 1 if ANSI function prototypes are usable.  */
+#define PROTOTYPES 1
+
+/* Define if you have the dgettext function.  */
+#define HAVE_DGETTEXT 1
+
+/* Define if you have the gettext function.  */
+#define HAVE_GETTEXT 1
+
+/* Define if you have the strerror function.  */
+#define HAVE_STRERROR 1
+
+/* Define if you have the <alloca.h> header file.  */
+#define HAVE_ALLOCA_H 1
+
+/* Define if you have the <libintl.h> header file.  */
+#define HAVE_LIBINTL_H 1
+
+/* Define if you have the <string.h> header file.  */
+/* #undef HAVE_STRING_H */
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
