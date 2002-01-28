5db93341594e63c6410c8d8138fe20a8832cbd6e
dankelley <dankelley>
2002-01-28 13:21:11 +0000

    *** empty log message ***

--- doc/sourceforge/index.php
+++ doc/sourceforge/index.php
@@ -263,7 +263,7 @@ Search gri manual for (may be a regular expression):<p>
         }
         echo ";";
 	?>
-      <p>pre-release #2 of 2.8.6 is <a href="./pre2/">here</a>.
+      pre-release #2 of 2.8.6 is <a href="./pre2/">here</a>.
       </td>
     </tr>
   </table>
