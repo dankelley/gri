4e097daab6a7e96e306fac579184045ac4e694ad
dankelley <dankelley>
2002-03-19 14:13:50 +0000

    *** empty log message ***

--- doc/sourceforge/index.php
+++ doc/sourceforge/index.php
@@ -265,7 +265,7 @@ Search gri manual for (may be a regular expression):<p>
         }
         echo ";<br>";
 	?>
-       pre-release #4 of 2.8.7 is <a href="./pre5/">here</a>.
+       pre-release #5 of 2.8.7 is <a href="./pre5/">here</a>.
       </td>
     </tr>
   </table>
