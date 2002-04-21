a37d28b808b865d92324533fae1302720a0a68ed
dankelley <dankelley>
2002-04-21 15:40:24 +0000

    hi

--- check.pl
+++ check.pl
@@ -4,6 +4,28 @@ use Date::Manip;
 #    http://theoryx5.uwinnipeg.ca/CPAN/data/DateManip/Manip.html
 # or other CPAN sites.
 
+sub indicate_age($$) {
+    my $before = $_[0];
+    my $item = $_[1];
+    $age = &DateCalc(&ParseDate("now"), $before);
+    $s = 0;			# prevent warning
+    ($y, $mo, $w, $d, $h, $min, $s) = split(/:/, "$age");
+    if ($y > 0) {
+	print "\t... $item was last updated $y years and $mo months ago\n";
+    } elsif ($mo > 0) {
+	print "\t... $item was last updated $mo months and $w weeks ago\n";
+    } elsif ($w > 0) {
+	print "\t... $item was last updated $w weeks and $d days ago\n";
+    } elsif ($d > 0) {
+	print "\t... $item was last updated $d days and $h hours ago\n";
+    } elsif ($h > 0) {
+	print "\t... $item was last updated $h hours and $min minutes ago\n";
+    } else {
+	print "\t... $item was last updated $mo months, $w weeks, $h hours and $min minutes ago\n";
+    }
+}
+
+
 $version_makefile = "?";
 
 print "Check 1: do the version numbers match?\n";
@@ -36,6 +58,7 @@ while(<DOC>) {
     }
 }
 die version_mismatch() if $version_makefile ne $version_doc;
+print "\t... Makefile and doc/gri.texim match\n";
 
 open(SPEC, "gri.spec") or die "Cannot open gri.spec";
 $version_spec = "?.?.?";
@@ -47,6 +70,7 @@ while(<SPEC>) {
     }
 }
 die version_mismatch() if $version_spec ne $version_makefile;
+print "\t... gri.spec also matches\n";
 
 open(CMD, "gri.cmd") or die "Cannot open gri.cmd";
 $version_cmd = "?.?.?";
@@ -57,13 +81,10 @@ s/.*version //;
 s/\)//;
 $version_cmd = $_;
 die version_mismatch() if $version_cmd ne $version_makefile;
-print "\t... yes\n";
+print "\t... gri.cmd also matches\n";
 
 #### #### #### #### #### #### #### #### #### 
-print "Check 2: have the various changelog files been updated recently? ... ";
-$now = &ParseDate("now");
-print "\n";
-#print "\t ... the present time is $now\n"; 
+print "Check 2: have various dated files been updated recently?\n";
 
 # ChangeLog
 open(CHANGELOG, "ChangeLog") or die "Can't open ChangeLog file";
@@ -71,20 +92,9 @@ $_ = <CHANGELOG>;
 close(CHANGELOG);
 @_ = split;
 $date_changelog = &ParseDate($_[0]);
-#print "\t ... the last update in 'ChangeLog' was on $date_changelog\n";
-$age_changelog = &DateCalc($now, $date_changelog);
-($y, $mo, $w, $d, $h, $min, $s) = split(/:/, "$age_changelog");
-if ($y > 0) {
-    print "\t... the Changelog file is more than a year old.  It needs to be updated\n";
-    die;
-}
-if ($mo > 0) {
-    print "\t... the Changelog file is $mo months, $w weeks, and $d days old\n";
-} else {
-    print "\t... the Changelog file is $w weeks, $d days, and $h hours old\n";
-}
+indicate_age($date_changelog, "       ChangeLog");
 
-# portion of gri.spec
+# gri.spec
 open(SPEC, "gri.spec") or die "Can't open gri.spec";
 while(<SPEC>) {
     next if !/^%changelog/;
@@ -93,49 +103,37 @@ while(<SPEC>) {
     @_ = split;
     #print "    (The date sequence is [$_[1] $_[2] $_[3] $_[4]].)\n";
     $date_spec = &ParseDate("$_[1] $_[2] $_[3] $_[4]");
-    $age_spec = &DateCalc($now, $date_spec);
-    ($y, $mo, $w, $d, $h, $min, $s) = split(/:/, "$age_spec");
-    if ($y > 0) {
-	print "\t... the gri.spec file is more than a year old.  It needs to be updated\n";
-	die;
-    }
-    if ($mo > 0) {
-	print "\t... the gri.spec file is $mo months, $w weeks, and $d days old\n";
-    } else {
-	print "\t... the gri.spec file is $w weeks, $d days, and $h hours old\n";
-    }
     last;
 }
-#print "    (The last update in 'gri.spec' was on $date_spec)\n";
-#print "  Age of gri.spec is $age_spec (yy:mm:wk:dd:hh:mm:ss)\n";
+indicate_age($date_spec, "        gri.spec");
 
 # debian/changelog
-# portion of gri.spec
-open(DEBIAN_CHANGELOG, "debian/changelog") 
-    or die "Can't open debian/changelog";
-print "\n";
-print "DEBUG.  Now examine debian/changelog file.\n";
-print "BUG: need to examine WHOLE file and find MOST RECENT date!\n";
-print "\n";
-print "For reference, the present date is $now\n";
+open(DEBIAN_CHANGELOG, "debian/changelog") or die "Can't open debian/changelog";
+#print "DEBUG.  Now examine debian/changelog file.\n";
+#print "BUG: need to examine WHOLE file and find MOST RECENT date!\n";
+#print "\n";
+#print "For reference, the present date is $now\n";
+$latest = &ParseDate("1971"); # pre-history, basically
 while(<DEBIAN_CHANGELOG>) {
     if (/ \-\- (.*) <(.*)> (.*), (.*)/) {
 	$date_debian_changelog = &ParseDate($4);
-	print "the sequence '$4' parses to $date_debian_changelog\n";
-	$age_debian_changelog = &DateCalc($now, $date_debian_changelog);
-	($y, $mo, $w, $d, $h, $min, $s) = split(/:/, "$age_debian_changelog");
-	if ($y > 0) {
-	    print "\t... the debian/changelog file is more than a year old.  It needs to be updated\n";
-	    die;
-	}
-	if ($mo > 0) {
-	    print "\t... the debian/changelog file is $mo months, $w weeks, and $d days old\n";
-	} else {
-	    print "\t... the debian/changelog file is $w weeks, $d days, and $h hours old\n";
-	}
-#	last; # do not break out of the loop, since the items may be misordered
+	#print "the sequence '$4' parses to $date_debian_changelog\n";
+	#print "latest $latest    $date_debian_changelog\n";
+	$latest = $date_debian_changelog if (&Date_Cmp($date_debian_changelog, $latest) > 0);
     }
 }
+$date_debian_changelog = $latest;
+indicate_age($date_debian_changelog, "debian/changelog");
 
 # doc/gri.texim
-print "  Age of doc/gri.texim is ...NOT CODED YET...\n";
+open (DOC, "doc/gri.texim") or die "Cannot open doc/gri.texim";
+$latest = &ParseDate("1971"); # pre-history, basically
+while(<DOC>) {
+    if (/\@subsection Version (.*) \[(.*)\]$/) {
+	#$version = $1;
+	$date_doc = $2;
+	$latest = $date_doc if (&Date_Cmp($date_doc, $latest) > 0);
+    }
+}
+$date_doc = $latest;
+indicate_age($date_doc, "   doc/gri.texim");
