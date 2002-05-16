6d5c132d9c117caf39558e13804741a647607069
dankelley <dankelley>
2002-05-16 22:50:46 +0000

    *** empty log message ***

--- gri.cmd
+++ gri.cmd
@@ -1,5 +1,5 @@
 #         gri - scientific graphic program (version 2.10.0)
-#              GPL Copyright 1991-2001 Dan E. Kelley.
+#              GPL Copyright 1991-2002 Dan E. Kelley.
 #
 # NOTE: The linkages to `extern "C"' routines makes use a list of C 
 # functions defined in the file tags.hh.
