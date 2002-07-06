bc7c499e45ac521568ae58ec207c714f6be05419
dankelley <dankelley>
2002-07-06 15:05:36 +0000

    *** empty log message ***

--- doc/sourceforge/bugs.php
+++ doc/sourceforge/bugs.php
@@ -6,12 +6,23 @@ print "- <A HREF=\"$sf_url/tracker/?atid=105511&group_id=$gri_group_id&func=brow
 set_up_rhs();
 ?>
 
+<h1>Known bugs</h1>
 <p>
-To browse bug reports, or to submit a bug report, go
 <?php
 print "<A HREF=\"$sf_url/tracker/?atid=105511&group_id=$gri_group_id&func=browse\">";
 ?>
-here</A>.<br>
+Bug reports</A> may be browsed and tracked online.
+</p>
+
+<h1>Report a bug</h1>
+
+<p>
+Please
+<?php
+print "<A HREF=\"$sf_url/tracker/?atid=105511&group_id=$gri_group_id&func=add&group_id=$gri_group_id&atid=105511\">";
+?>
+report a bug</A> if you find one, to help yourself and to help other Gri users.
+</p>
 
 <?php
 footer();
