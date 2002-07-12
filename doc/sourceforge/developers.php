bcdbbf8e830a8f0fc4feb1ec88851421acee1976
dankelley <dankelley>
2002-07-12 13:49:15 +0000

    Add to-do list.

--- doc/sourceforge/developers.php
+++ doc/sourceforge/developers.php
@@ -37,6 +37,7 @@ print "<A HREF=\"$sf_url/forum/forum.php?forum_id=16975\">help</A>.<br>\n";
 print "<A HREF=\"$sf_url/tracker/?atid=35$gri_group_id&group_id=$gri_group_id&func=browse\">Feature requests</A><br>\n";
 print "<A HREF=\"$sf_url/tracker/?atid=10$gri_group_id&group_id=$gri_group_id&func=browse\">Bug reports</A><br>\n";
 print "<A HREF=\"$sf_url/tracker/?atid=30$gri_group_id&group_id=$gri_group_id&func=browse\">Patch submissions</A><br>\n";
+print "<A HREF=\"$sf_url/pm/task.php?group_project_id=8706&group_id=$gri_group_id&func=browse\">View to-do list</A><br>\n";
 print "<A HREF=\"$sf_url/cvs/?group_id=$gri_group_id\">Get CVS source</a><br>\n";
 print "<A HREF=\"http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/gri/gri/\">View CVS history</a><br>\n";
 ?>
