1ff53205243efe5ec8ab676c1e5d74f51724c45d
dankelley <dankelley>
2002-12-07 13:29:35 +0000

    explain new use for 'pending'

--- doc/sourceforge/bugs.php
+++ doc/sourceforge/bugs.php
@@ -10,8 +10,13 @@ set_up_rhs();
 <h1>Known bugs</h1>
 <p>
 The details of
-<?php print "<A HREF=\"$sf_url/tracker/?atid=105511&amp;group_id=$gri_group_id&amp;func=browse\">";?>
-known bugs</A> may be browsed online.  Be sure to check both open and closed bugs.
+<?php print "<A HREF=\"$sf_url/tracker/?atid=105511&amp;group_id=$gri_group_id&amp;atid=105511\">";?>
+known bugs</A> may be browsed online.  Bugs that apply have been fixed
+in the CVS version, but not in the most recent release, are denoted
+<b>pending</b>.  Bugs that have not yet been fixed are denoted
+<b>open</b>.  Bugs that existed before the present release are denoted
+<b>closed</b>.
+
 </p>
 
 <h1>Report a bug</h1>
