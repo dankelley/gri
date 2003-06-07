908e17dafadb5a047cd2ef43f164bd2b35100179
dankelley <dankelley>
2003-06-07 23:04:06 +0000

    *** empty log message ***

--- src/version.cc
+++ src/version.cc
@@ -1,40 +1,4 @@
 // NB: number string must agree with line 1 of gri.cmd
-char _gri_number[] = "2.12.5";
-char _gri_release_time[] = "2003-May-19";
+char _gri_number[] = "2.12.6";
+char _gri_release_time[] = "2003-Jun-07";
 char _gri_date[] = "2003";
-
-// OLDER VERSIONS:
-//
-//  2.12.5      2002-may-19 Victoria Day (Canada)
-//  2.12.4      2003-apr-06
-//  2.12.3      2003-mar-01
-//  2.12.2      2003-feb-07 Munro Day (Dalhousie University)
-//  2.12.1      2002-sep-25
-//  2.12.0      2002-sep-15 Terry Fox Day (Canada)
-//  2.11.0      ...
-//  2.10.1      2002-jun-01
-//  2.10.0      2002-may-20 Victoria Day (Canada)
-//  2.8.6       2002-Feb-14
-//  2.8.5       2001-Dec-13
-//  2.8.4       2001-Oct-04
-//  2.8.2       2001-Sep-17
-//  2.8.1       2001-Sep-07
-//  2.8.0       2001-Jul-30
-//  2.6.1       2001-May-11
-//  2.6.0       2001-Apr-01
-//  2.5.5       2000-Jun-21
-//  2.5.4       2000-Jun-01
-//  2.5.3       2000-May-22
-//  2.5.2       2000-May-20
-//  2.5.1       2000-May-09
-//  2.4.4       2000-May-07
-//  2.4.3       2000-Apr-01
-//  2.4.2       2000-Mar-25
-//  2.4.1       2000-Jan-31
-//  2.4.0=2.3.7 2000-Jan-05
-//  2.4.0=2.3.7 2000-Jan-05
-//  2.3.7       1999-Dec-14
-//  2.3.6       1999-Dec-13
-//  2.3.5       1999-Dec-12
-//  2.2.6       1999-Nov-29
-//  2.2.5       1999-Nov-10
