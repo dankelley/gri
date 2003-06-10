a8a39c525025fbb89cf480f225542de7fa8e823c
dankelley <dankelley>
2003-06-10 14:39:10 +0000

    *** empty log message ***

--- src/version.cc
+++ src/version.cc
@@ -1,4 +1,4 @@
 // NB: number string must agree with line 1 of gri.cmd
-char _gri_number[] = "2.12.6";
+char _gri_number[] = VERSION;	// set in Makefile
 char _gri_release_time[] = "2003-Jun-07";
 char _gri_date[] = "2003";
