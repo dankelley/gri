fd811009cc2c26f6e9ce4592af1f539186d37d2a
dankelley <dankelley>
2005-06-04 21:15:22 +0000

    Hm, haven't updated the date in a year!

--- src/version.cc
+++ src/version.cc
@@ -1,4 +1,4 @@
 // NB: number string must agree with line 1 of gri.cmd
 char _gri_number[] = VERSION;	// set in Makefile
-char _gri_release_time[] = "2004 Dec 13";
-char _gri_date[] = "2004";
+char _gri_release_time[] = "2005 Jun 4";
+char _gri_date[] = "2005";
