438e367439954368cbdf9c857dd3bb60b4075871
dankelley <dankelley>
2005-12-18 14:34:44 +0000

    Version number and xcode confusion.

--- src/version.cc
+++ src/version.cc
@@ -1,5 +1,16 @@
-// NB: number string must agree with line 1 of gri.cmd
-#define stringify(x) # x
-char _gri_number[] = stringify(VERSION);	// set in Makefile
+// NB: _gri_number will be checked against line 1 of gri.cmd
+#ifdef OSX_BUNDLE
+char _gri_number[] = PACKAGE_VERSION;
+#else
+// Don't ask me why, but I used to do
+//        #define stringify(x) # x
+//        char _gri_number[] = stringify(VERSION);
+// here.  I think I was messing with the OSX bundle release,
+// but whatever I was doing, it sems not to be needed.  I'll
+// just leave it here for a while, though.
+// DEK 2005-12-18
+//
+char _gri_number[] = PACKAGE_VERSION;
+#endif
 char _gri_release_time[] = "2005 Jun 4";
 char _gri_date[] = "2005";
