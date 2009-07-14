fbf8ccf047ec6b550d80afd293ff40df3cd2e8a7
dankelley <dankelley>
2009-07-14 12:31:44 +0000

    update to version 2.12.19

--- src/gri.cmd
+++ src/gri.cmd
@@ -1,4 +1,4 @@
-#         gri - scientific graphic program (version 2.12.18)
+#         gri - scientific graphic program (version 2.12.19)
 #         Copyright 2009 by Dan E. Kelley; GPLv2+ licensing.
 #
 # NOTE: The linkages to `extern "C"' routines makes use a list of C 
