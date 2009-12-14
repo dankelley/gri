06ec9caf36783f2429d8b947ba1d14f6b1d769de
Dan Kelley <kelley@remit.local>
2009-12-14 18:07:22 -0400

    improve webpage/developers

--- doc/sourceforge/developers.php
+++ doc/sourceforge/developers.php
@@ -7,13 +7,11 @@ set_up_rhs();
 
 <h1>Developers</h1>
 
-<p>
-<a href="<?php print "$sf_url/users/dankelley/";?>">Dan Kelley</a>
-wrote Gri, and
-<a href="<?php print "$sf_url/users/psg/";?>">Peter Galbraith</a>
-wrote the Gri Emacs mode.  The two have collaborated on Gri development 
-for a decade, and have enjoyed the help and advice of many users, named
-in the Gri manual.
+<p> <a href="<?php print "$sf_url/users/dankelley/";?>">Dan Kelley</a>
+wrote Gri, and <a href="<?php print "$sf_url/users/psg/";?>">Peter
+Galbraith</a> wrote the Gri Emacs mode.  The two have collaborated on
+Gri development (and science) for nearly two decades, and have enjoyed
+the help and advice of many users, named in the Gri manual.
 
 </p>
 
@@ -41,16 +39,9 @@ print "<br>";
 print "Trackers: ";
 print "[<A HREF=\"$sf_url/tracker/?atid=35$gri_group_id&amp;group_id=$gri_group_id&amp;func=browse\">Feature requests</A>] ";
 print "[<A HREF=\"$sf_url/tracker/?atid=10$gri_group_id&amp;group_id=$gri_group_id&amp;func=browse\">Bug reports</A>] ";
-print "[<A HREF=\"$sf_url/tracker/?atid=30$gri_group_id&amp;group_id=$gri_group_id&amp;func=browse\">Patch submissions</A>]\n";
-print "<br> Dan Kelley:";
-print "[<a HREF=\"$sf_url/developer/diary.php?diary_user=31559\">Diary</a>]\n";
-print "[<A HREF=\"$sf_url/pm/task.php?group_project_id=8706&amp;group_id=$gri_group_id&amp;func=browse\">To-do list</A>].\n";
-print "<br>";
-#print "CVS:\n";
-#print "[<A HREF=\"$sf_url/cvs/?group_id=$gri_group_id\">Get CVS source</a>]\n";
-#print "[<A HREF=\"http://gri.cvs.sourceforge.net/gri/gri/\">View CVS history</a>].\n";
-print "<p>Git:\n";
-print "[<A HREF=\"http://gri.git.sourceforge.net/git/gitweb.cgi?p=gri/gri;a=summary\">summary</a>]<br>";
+print "[<A HREF=\"$sf_url/tracker/?atid=30$gri_group_id&amp;group_id=$gri_group_id&amp;func=browse\">Patch submissions</A>]<br>\n";
+print "<h1>Developer Work Cycle</h1>\n";
+print "[<A HREF=\"http://gri.git.sourceforge.net/git/gitweb.cgi?p=gri/gri;a=summary\">Git summary</a>]<br>";
 print "Do this one time:<br>";
 print "<tt>git clone ssh://dankelley@gri.git.sourceforge.net/gitroot/gri/gri</tt></p>\n";
 print "Work cycle:<br>\n";
