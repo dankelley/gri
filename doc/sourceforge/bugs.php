4337db851e19d8a0e82ae9f5fbc6577a6871c6e2
Dan Kelley <kelley.dan@gmail.com>
2010-04-09 16:48:52 -0300

    fix link for bug reporting

--- doc/sourceforge/bugs.php
+++ doc/sourceforge/bugs.php
@@ -10,8 +10,7 @@ set_up_rhs();
 <h1>Known bugs</h1>
 <p>
 The details of
-<?php print "<A HREF=\"$sf_url/tracker/?atid=105511&amp;group_id=$gri_group_id&amp;atid=105511\">";?>
-known bugs</A> may be browsed online.
+<?php print "<A HREF=\"$sf_url/tracker/?limit=25&func=&group_id=5511&atid=105511&assignee=&status=&category=&artgroup=&keyword=&submitter=&artifact_id=&assignee=&status=1&category=&artgroup=&submitter=&keyword=&artifact_id=&submit=Filter&mass_category=&mass_priority=&mass_resolution=&mass_assignee=&mass_artgroup=&mass_status=&mass_cannedresponse=\">";?>known bugs</A> may be browsed online.
 </p>
 
 <h1>Report a bug</h1>
