16ef6598eeed54f39db018abbc29f5621201a232
Dan Kelley <kelley.dan@gmail.com>
2010-12-08 14:24:09 -0400

    bump version number

--- src/gri.cmd
+++ src/gri.cmd
@@ -1,4 +1,4 @@
-#         gri - scientific graphic program (version 2.12.22)
+#         gri - scientific graphic program (version 2.12.23)
 #         Copyright 2010 by Dan E. Kelley; GPLv2+ licensing.
 #
 # NOTE: The linkages to `extern "C"' routines makes use a list of C 
